f7fc27d067423c09adf9308ff833092e165f5bc5
tomotomo <tomotomobile@gmail.com>
2021-05-15 15:52:21 +0900

    日本語化､Favicon変更

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import React from 'react';
 const dom = document.getElementById('root');
 
 const Main = (_props) => {
-  return <h1>CoderDojo Gotanda</h1>
+  return (
+    <section>
+      <h1>CoderDojo 五反田</h1>
+      <p><a href="https://coderdojo-gotanda.doorkeeper.jp/">参加する</a></p>
+    </section>
+  );
 };
 
 ReactDOM.render(<Main />, dom);
