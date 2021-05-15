671c7d8bd6a8af6bf630988179cddefdf119b2b2
tomotomo <tomotomobile@gmail.com>
2021-05-15 16:16:10 +0900

    add image

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ const Main = (_props) => {
   return (
     <section>
       <h1>CoderDojo 五反田</h1>
+      <img src={`${process.env.PUBLIC_URL}/images/sceen_offline.jpg`} style={{maxWidth:480}} />
       <p><a href="https://coderdojo-gotanda.doorkeeper.jp/">参加する</a></p>
     </section>
   );
 };
+
 
 ReactDOM.render(<Main />, dom);
